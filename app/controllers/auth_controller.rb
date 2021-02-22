4ee4a6b5f20d221f81336c9fb29418f581f7ffbc
mertoz41 <mertozka41@gmail.com>
2021-02-22 10:48:59 -0500

    login functionality implemented with jwt

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,12 @@
+class AuthController < ApplicationController
+    def create
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            payload = {user_id: @user.id}
+            token = encode(payload)
+            render json: {user: UserSerializer.new(@user), token: token}
+        else 
+            render json: {message: 'Invalid username or password.'}
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   # post '/experiment', to: 'users#experiment'
   get '/experiment', to: 'users#experiment'
+  post '/login', to: 'auth#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
