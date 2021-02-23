90c78d776a68d357d274b9b1b2637c65ff320447
mertoz41 <mertozka41@gmail.com>
2021-02-23 11:23:37 -0500

    information regarding post implemented on the post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,9 +1,38 @@
 class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :user_id, :genre_id, :instrument_id, :song_id, :artist_id, :clip
+  attributes :id, :user_id, :genre_id, :instrument_id, :song_id, :artist_id, :clip, :created_at, :user_name, :artist_name, :song_name, :genre_name, :instrument_name 
 
   def clip
     url_for(object.clip)
   end
+
+  def created_at
+    object.created_at.to_date
+  end
+
+  def user_name
+    user = User.find(object.user_id)
+    return user.username
+  end
+
+  def artist_name
+    artist = Artist.find(object.artist_id)
+    return artist.name
+  end 
+
+  def song_name
+    song = Song.find(object.song_id)
+    return song.name
+  end 
+
+  def genre_name
+    genre = Genre.find(object.genre_id)
+    return genre.name 
+  end 
+  def instrument_name
+    instrument = Instrument.find(object.instrument_id)
+    return instrument.name 
+  end 
+
 end
