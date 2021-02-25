2a71a47245822ad01f44052c389fff4f37430cee
mertoz41 <mertozka41@gmail.com>
2021-02-25 10:22:58 -0500

    follow and unfollow functionality implemented

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,11 @@
 class FollowsController < ApplicationController
+    def create 
+        new_follow = Follow.create(follower_id: params[:followerId], followed_id: params[:followedId])
+        render json: {follow: new_follow}
+    end 
+    def destroy
+        follow = Follow.find(params[:id])
+        follow.destroy
+        render json: {message: "success"}
+    end 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :username, :created_at, :avatar
+  attributes :id, :username, :created_at, :avatar, :follows, :followed_by
   attribute :avatar, if: -> {object.avatar.present?}
 
   def created_at
@@ -10,4 +10,32 @@ class UserSerializer < ActiveModel::Serializer
   def avatar
     return url_for(object.avatar)
   end 
+
+  # def follows
+  #   # need the names of followed_id
+  #   follows_arr = []
+  #   object.follows.each do |follow|
+  #     obj = {}
+  #     obj = follow.attributes
+  #     user = User.find(follow.followed_id)
+  #     obj["name"] = user.username
+  #     obj["avatar"] = url_for(user.avatar)
+  #     follows_arr.push(obj)
+  #   end 
+  #   return follows_arr
+
+  # end 
+  # def followed_by
+  #   # need the names of follower_id
+  #   followed_by_arr = []
+  #   object.followed_by.each do |follow|
+  #     obj = {}
+  #     obj = follow.attributes
+  #     user = User.find(follow.follower_id)
+  #     obj["name"] = user.username
+  #     obj["avatar"] = url_for(user.avatar)
+  #     followed_by_arr.push(obj)
+  #   end 
+  #   return followed_by_arr
+  # end 
 end
