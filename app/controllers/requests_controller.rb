ba1d0a5cf8ff48c88eda602f8d464900c2046179
mertoz41 <mertozka41@gmail.com>
2021-03-01 14:41:40 -0500

    new request handled in the backend with serializer

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,11 @@
 class RequestsController < ApplicationController
+    def create
+        user = User.find(params[:user_id])
+        byebug
+        artist = Artist.find_or_create_by(name: params[:artistName])
+        song = Song.find_or_create_by(name: params[:songName], artist_id: artist.id)
+        @new_request = Request.create(user_id: user.id, artist_id: artist.id, song_id: song.id, fulfilled: false)
+
+        render json: @new_request, serializer: RequestSerializer
+        end
 end
