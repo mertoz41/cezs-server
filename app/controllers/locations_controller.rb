4d3db9f54496de6082ce8e6f339f7d351b31e61c
mertoz41 <mertozka41@gmail.com>
2021-03-02 12:07:51 -0500

    locations serializer updated for markers to display user info

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,8 @@
 class LocationsController < ApplicationController
     def index
-        locations = Location.all
-        render json: {locations: locations}
+        @locations = Location.all
+        render json: {locations: ActiveModel::Serializer::CollectionSerializer.new(@locations, each_serializer: LocationSerializer)}
+        # how can all locations except current users be shown so that loggedin users location isnt filtered out post process.
     end 
     def create
         @user = User.find(params[:user_id])
--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,3 +1,13 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :latitude, :longitude, :user_id
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :latitude, :longitude, :user_id, :avatar, :username
+  def avatar
+    user = User.find(object.user_id)
+    return url_for(user.avatar)
+  end
+  def username
+    user = User.find(object.user_id)
+    return user.username
+  end 
 end
