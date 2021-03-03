64fadaf6a3d032e05754c9bcde22b7a401302cbf
mertoz41 <mertozka41@gmail.com>
2021-03-03 10:39:15 -0500

    show action to send all users belonging to that location implemented for when a location is clicked

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,11 @@ class LocationsController < ApplicationController
         locations = Location.all
         render json: {locations: locations}
         # how can all locations except current users be shown so that loggedin users location isnt filtered out post process.
-    end 
+    end
+    def show
+        @users = Location.find(params[:id]).users
+        render json: {users: ActiveModel::Serializer::CollectionSerializer.new(@users, each_serializer: UserSerializer)}
+    end
     def create
         # incoming location to be checked whether it exists
         # if it exists, create Userlocation model with that locations id and users id
