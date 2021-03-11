74d5e525209d872bcbe1279c58be795083933427
mertoz41 <mertozka41@gmail.com>
2021-03-11 08:32:00 -0500

    follows and followers actions updated with o of 1

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -11,22 +11,14 @@ class FollowsController < ApplicationController
 
     def follows
         user = User.find(params[:id])
-        @followed_users = []
-        user.follows.each do |follow|
-            searched_user = User.find(follow.followed_id)
-            @followed_users.push(searched_user)
-        end 
+        @followed_users = user.followeds
         render json: {follows: ActiveModel::Serializer::CollectionSerializer.new(@followed_users, each_serializer: UserSerializer)}
 
         # render json: {follows: ActiveModel::Serializer::CollectionSerializer.new(@follows, each_serializer: FollowSerializer)}
     end 
     def followers
         user = User.find(params[:id])
-        @followed_by_users = []
-        user.followed_by.each do |follow|
-            searched_user = User.find(follow.follower_id)
-            @followed_by_users.push(searched_user)
-        end
+        @followed_by_users = user.followers
         render json: {followers: ActiveModel::Serializer::CollectionSerializer.new(@followed_by_users, each_serializer: UserSerializer)}
     end
 end
