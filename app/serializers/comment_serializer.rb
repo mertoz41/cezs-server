1ca25dbcba88e8ae658293164be700e6a92c82d9
mertoz41 <mertozka41@gmail.com>
2021-03-13 18:27:44 -0500

    created at included in the comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,11 +1,14 @@
 class CommentSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :comment, :user_id, :post_id, :username, :avatar
+  attributes :id, :comment, :user_id, :post_id, :username, :avatar, :created_at
   def username
     user = User.find(object.user_id)
     return user.username
   end
+  def created_at
+    object.created_at.to_date
+  end 
   def avatar
     user = User.find(object.user_id)
     return url_for(user.avatar)
