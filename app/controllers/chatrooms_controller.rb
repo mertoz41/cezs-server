99d85a24dd4b01d8f70417b4bff094d66a3d30b4
mertoz41 <mertozka41@gmail.com>
2021-03-25 19:38:54 -0400

    title removed from a chatroom, serializer being worked on

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,15 +5,16 @@ class ChatroomsController < ApplicationController
     render json: {messages: chatroom.messages}
   end
   def create
-      chatroom = Chatroom.find_or_create_by(title: params[:title])
+      chatroom = Chatroom.create
       user = User.find(params[:user_id])
       receiving_user = User.find(params[:receiver_id])
       user_chatroom = Userchatroom.find_or_create_by(chatroom_id: chatroom.id, user_id: user.id)
       receiving_chatroom = Userchatroom.find_or_create_by(chatroom_id: chatroom.id, user_id: receiving_user.id)
       message = Message.create(content: params[:message], user_id: user.id, chatroom_id: chatroom.id)
       serialized_data = ActiveModelSerializers::Adapter::Json.new(MessageSerializer.new(message)).serializable_hash
-      ActionCable.server.broadcast "chatrooms_channel_#{chatroom.id}", serialized_data
-      head :ok
+      render json: {chatroom: chatroom}
+      # ActionCable.server.broadcast "chatrooms_channel_#{chatroom.id}", serialized_data
+      # head :ok
     # chatroom = Chatroom.create(title: params[:title])
     # ActionCable.server.broadcast "chatrooms_channel", chatroom
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_164914) do
+ActiveRecord::Schema.define(version: 2021_03_25_231954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2021_03_25_164914) do
   create_table "chatrooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "title"
   end
 
   create_table "comments", force: :cascade do |t|
