edcb44d8986ac564ea23b7f293ffa69b9a0fde9c
mertoz41 <mertozka41@gmail.com>
2021-03-25 09:32:45 -0400

    show action created in chatroom to display all messages

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,9 @@
 class ChatroomsController < ApplicationController
+
+  def show
+    chatroom = Chatroom.find(params[:id])
+    render json: {messages: chatroom.messages}
+  end
   def create
     chatroom = Chatroom.create(title: params[:title])
     ActionCable.server.broadcast "chatrooms_channel", chatroom
