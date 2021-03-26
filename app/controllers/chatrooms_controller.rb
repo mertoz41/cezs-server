8d2cf4c3da4b1f1a1e16f688ff77e10d48d1d9de
mertoz41 <mertozka41@gmail.com>
2021-03-26 17:08:32 -0400

    chatroom messages come in ascending order

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,7 +2,8 @@ class ChatroomsController < ApplicationController
 
   def show
     chatroom = Chatroom.find(params[:id])
-    render json: {messages: chatroom.messages}
+    ordered = chatroom.messages.order(created_at: :asc)
+    render json: {messages: ordered}
   end
   def create
       chatroom = Chatroom.create
