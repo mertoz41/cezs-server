ae2a481abd7583af372b303874872a0f1fc09918
mertoz41 <mertozka41@gmail.com>
2021-03-29 10:17:28 -0400

    genre taken out of post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,35 +3,20 @@ class PostsController < ApplicationController
     def create
         user_id = params[:user_id].to_i
         instrument_id = params[:instrument_id].to_i
-        genre_id = params[:genre_id].to_i
         artist_name = params[:artist_name]
         artist_id = params[:artist_id]
         song_name = params[:song_name]
         artist = Artist.find_or_create_by(name: artist_name, spotify_id: artist_id, avatar: params[:artist_pic])
         song = Song.find_or_create_by(name: song_name, artist_id: artist.id)
         
-        @post = Post.create(user_id: user_id, instrument_id: instrument_id, genre_id: genre_id, artist_id: artist.id, song_id: song.id)
+        @post = Post.create(user_id: user_id, instrument_id: instrument_id, artist_id: artist.id, song_id: song.id)
         @post.clip.attach(params[:clip])
         render json: @post, serializer: PostSerializer
     end 
 
     def filter
-        # list = params[:selectedItems]
-        instruments = params[:instruments]
-        genres = params[:genres]
-        by_instrument = Post.where(instrument_id: instruments)
-        by_genre = Post.where(genre_id: genres)
-        # if instruments.length < 1 || genres.length < 1
-   
-        #     @filtered_timeline = @filtered_timeline + by_instrument + by_genre
-        # else
-        #     all = Post.where(instrument_id: instruments, genre_id: genres)
-        #     @filtered_timeline = @filtered_timeline + all
-
-        # end
-        @filtered_timeline = by_instrument + by_genre 
-    
-        
+        instruments = params[:selected_instruments]
+        @filtered_timeline  = Post.where(instrument_id: instruments)
         render json: {timeline: ActiveModel::Serializer::CollectionSerializer.new(@filtered_timeline, each_serializer: PostSerializer)}
 
     end
