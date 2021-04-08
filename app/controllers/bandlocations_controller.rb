47836dfed2414ff2a9834e4a23820e56f0540398
mertoz41 <mertozka41@gmail.com>
2021-04-08 11:14:13 -0400

    band updating location implemented i think

--- app/controllers/bandlocations_controller.rb
+++ app/controllers/bandlocations_controller.rb
@@ -3,9 +3,8 @@ class BandlocationsController < ApplicationController
     def update
         @band = Band.find(params[:band_id])
         location = Location.find_or_create_by(city: params[:city], latitude: params[:latitude], longitude: params[:longitude])
-        byebug
-        # band_location = Bandlocation.find_by(band_id: @band.id)
-        # band_location.update(location_id: location.id)
-        # render json: {band: BandSerializer.new(@band)}
+        band_location = Bandlocation.find_by(band_id: @band.id)
+        band_location.update(location_id: location.id)
+        render json: {band: BandSerializer.new(@band)}
     end
 end
