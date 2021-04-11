4d20ba98ed56526681a344ca774346ecb10981bc
mertoz41 <mertozka41@gmail.com>
2021-04-11 14:04:23 -0400

    user followed artists, bands and chatrooms included in login action

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -7,6 +7,14 @@ class AuthController < ApplicationController
             @timeline = []
             @posts = []
             @shares = @user.shares
+            chatrooms = []
+            @user.chatrooms.each do |room|
+                obj = {}
+                obj = room.attributes
+                @room_users = room.users
+                obj['users'] = ActiveModel::Serializer::CollectionSerializer.new(@room_users, each_serializer: UserSerializer)
+                chatrooms.push(obj)
+            end 
 
             @user.posts.each do |post|
                 @posts.push(post)
@@ -16,9 +24,22 @@ class AuthController < ApplicationController
                 user.posts.each do |post|
                 @timeline.push(post)
                 end 
+            end
+            @user.followedbands.each do |band|
+                band.bandposts.each do |bandpost|
+                    @timeline.push(bandpost)
+                end
+            end 
+            @user.followedartists.each do |artist|
+                artist.posts.each do |post|
+                    @timeline.push(post)
+                end
+                artist.bandposts.each do |post|
+                    @timeline.push(post)
+                end
             end 
 
-            render json: {user: UserSerializer.new(@user), token: token, shares: ActiveModel::Serializer::CollectionSerializer.new(@shares, each_serializer: ShareSerializer),timeline: ActiveModel::Serializer::CollectionSerializer.new(@timeline, each_serializer: PostSerializer), posts: ActiveModel::Serializer::CollectionSerializer.new(@posts, each_serializer: PostSerializer)}
+            render json: {user: UserSerializer.new(@user), token: token, shares: ActiveModel::Serializer::CollectionSerializer.new(@shares, each_serializer: ShareSerializer),timeline: ActiveModel::Serializer::CollectionSerializer.new(@timeline, each_serializer: PostSerializer), posts: ActiveModel::Serializer::CollectionSerializer.new(@posts, each_serializer: PostSerializer), chatrooms: chatrooms}
         else 
             render json: {message: 'Invalid username or password.'}
         end
@@ -40,6 +61,14 @@ class AuthController < ApplicationController
         @user.followedbands.each do |band|
             band.bandposts.each do |bandpost|
                 @timeline.push(bandpost)
+            end
+        end 
+        @user.followedartists.each do |artist|
+            artist.posts.each do |post|
+                @timeline.push(post)
+            end
+            artist.bandposts.each do |post|
+                @timeline.push(post)
             end
         end 
         @shares = @user.shares
