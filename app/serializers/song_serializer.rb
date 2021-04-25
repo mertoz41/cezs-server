9f6466ec87534eee26be5a18c2c15745586c4c49
mertoz41 <mertozka41@gmail.com>
2021-04-24 21:49:02 -0400

    songs serializer updated

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,9 +1,15 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :name, :artist_name, :post_count
+  attributes :id, :name, :artist_name, :post_count, :user_count
+  has_many :posts
+  has_many :bandposts
+  
   def artist_name
     return object.artist.name
   end
   def post_count
     return object.posts.size
-  end 
+  end
+  def user_count
+    return object.users.size
+  end
 end
