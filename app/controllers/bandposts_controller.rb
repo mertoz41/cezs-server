4f853efb294bf94ace1920b1f74e2e394d984ed1
mertoz41 <mertozka41@gmail.com>
2021-04-29 12:11:43 -0400

    bandpost create flow updated

--- app/controllers/bandposts_controller.rb
+++ app/controllers/bandposts_controller.rb
@@ -5,14 +5,29 @@ class BandpostsController < ApplicationController
         spotify_id = params[:spotify_id]
         song_name = params[:song_name]
         instruments = JSON.parse params[:instruments]
-        artist = Artist.find_or_create_by(name: artist_name, spotify_id: spotify_id, avatar: params[:artist_pic])
-        song = Song.find_or_create_by(name: song_name, artist_id: artist.id)
-        @bandpost = Bandpost.create(band_id: band_id, artist_id: artist.id, song_id: song.id)
-        instruments.each do |inst|
-            Bandpostinstrument.create(instrument_id: inst, bandpost_id: @bandpost.id)
-        end
-        @bandpost.clip.attach(params[:clip])
-        render json: @bandpost, serializer: BandpostSerializer
+        artist = Artist.find_by(name: artist_name, spotify_id: spotify_id)
+        if artist
+            song = Song.find_or_create_by(name: song_name, artist_id: artist.id)
+            @bandpost = Bandpost.create(band_id: band_id, artist_id: artist.id, song_id: song.id)
+            instruments.each do |inst|
+                Bandpostinstrument.create(instrument_id: inst, bandpost_id: @bandpost.id)
+            end
+            @bandpost.clip.attach(params[:clip])
+            render json: @bandpost, serializer: BandpostSerializer
+        else
+            new_artist = Artist.create(name: artist_name, spotify_id: spotify_id, avatar: params[:artist_pic])
+            song = Song.create(name: song_name, artist_id: new_artist.id)
+            @bandpost = Bandpost.create(band_id: band_id, artist_id: artist.id, song_id: song.id)
+            instruments.each do |inst|
+                Bandpostinstrument.create(instrument_id: inst, bandpost_id: @bandpost.id)
+            end
+            @bandpost.clip.attach(params[:clip])
+            render json: @bandpost, serializer: BandpostSerializer
+        end 
+
+
+
+        # artist = Artist.find_or_create_by(name: artist_name, spotify_id: spotify_id, avatar: params[:artist_pic])
     end
 
     def share
