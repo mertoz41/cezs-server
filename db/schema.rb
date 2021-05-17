73ba2dfce8061c63ea4084d13541580c5856292b
mertoz41 <mertozka41@gmail.com>
2021-05-17 15:20:05 -0400

    genre completely removed

--- db/schema.rb
+++ db/schema.rb
@@ -192,12 +192,6 @@ ActiveRecord::Schema.define(version: 2021_05_04_154036) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "instruments", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
