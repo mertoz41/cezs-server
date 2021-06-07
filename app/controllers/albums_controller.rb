83598bf089d475183381357b3e72b32261d3c696
mertoz41 <mertozka41@gmail.com>
2021-06-07 13:54:31 -0400

    check album action created to see all posts under an album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,10 @@
 class AlbumsController < ApplicationController
     def albumcheck
-        byebug
         album = Album.find_by(spotify_id: params[:id])
-        
+        if album
+            render json: {songs: ActiveModel::Serializer::CollectionSerializer.new(album.songs, each_serializer: SongSerializer)}
+        else
+            render json: {message: 'album not found'}
+        end
     end
 end
