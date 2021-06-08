406dc166addf01b9b3d156226828e7b0d968ebaa
mertoz41 <mertozka41@gmail.com>
2021-06-08 13:16:42 -0400

    small fixes

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,6 +1,8 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :id, :name, :spotify_id, :artist_name
+  attributes :id, :name, :spotify_id, :artist_name, :artist_id
   def artist_name
     return object.artist.name
   end
+ 
+  
 end
