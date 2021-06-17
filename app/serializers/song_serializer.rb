095da65c50478c94b827e718c2cb444a0233e888
mertoz41 <mertozka41@gmail.com>
2021-06-17 00:18:17 -0400

    favoriteusers count added on song serializer

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,5 +1,5 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :name, :artist_name, :post_count, :user_count, :bandpost_count, :spotify_id, :album_name
+  attributes :id, :name, :artist_name, :post_count, :user_count, :bandpost_count, :spotify_id, :album_name, :favoriteusers_count
   has_many :posts
   has_many :bandposts
   
@@ -17,6 +17,9 @@ class SongSerializer < ActiveModel::Serializer
   end
   def album_name
     return object.album.name
+  end
+  def favoriteusers_count
+    return object.favoriteusers.size
   end
 end
 
