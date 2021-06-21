e89538d7f0ce5ad634db6197befed269969e3eb2
mertoz41 <mertozka41@gmail.com>
2021-06-20 21:59:40 -0400

    delete action updated

--- app/controllers/userinfluences_controller.rb
+++ app/controllers/userinfluences_controller.rb
@@ -1,17 +1,17 @@
 class UserinfluencesController < ApplicationController
     def create
-        @user = User.find(params[:user_id])
+        user = User.find(params[:user_id])
         spotify_id = params[:spotify_id]
         artist_name = params[:artist_name]
-        artist = Artist.find_or_create_by(name: artist_name, spotify_id: spotify_id)
-        user_influence = Userinfluence.create(user_id: @user.id, artist_id: artist.id)
-        render json: {user: UserSerializer.new(@user)}
+        @artist = Artist.find_or_create_by(name: artist_name, spotify_id: spotify_id)
+        user_influence = Userinfluence.create(user_id: user.id, artist_id: @artist.id)
+        render json: {artist: ArtistSerializer.new(@artist)}
     end
 
     def delete
         @user = User.find(params[:user_id])
         influence = Userinfluence.find_by(user_id: @user.id, artist_id: params[:artist_id])
         influence.destroy
-        render json: {user: UserSerializer.new(@user)}
+        render json: {message: 'influence removed'}
     end 
 end
