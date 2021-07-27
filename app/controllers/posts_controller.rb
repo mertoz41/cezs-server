bb6d12b675c329620c6d778f8f3ebb6f2226e674
mertoz41 <mertozka41@gmail.com>
2021-07-26 23:08:37 -0400

    post create action optimized

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,202 +2,37 @@ class PostsController < ApplicationController
 
     def create
         user_id = params[:user_id].to_i
-        # instrument_id = params[:instrument_id].to_i
-        byebug
-        # genre_id = params[:genre_id].to_i
         artist_name = params[:artist_name]
         artist_spotify_id = params[:artist_spotify_id]
         song_spotify_id = params[:song_spotify_id]
         song_name = params[:song_name]
         album_name = params[:album_name]
         album_spotify_id = params[:album_spotify_id]
-
         features = JSON.parse params[:features]
 
-        
-         
-
-        # find artist by artist name and spotify id
-        # if artist found use it for post creation
-        artist = Artist.find_by(name: artist_name, spotify_id: artist_spotify_id)
-        if artist
-
-
-
-
-            artist_album = Album.find_by(spotify_id: album_spotify_id, artist_id: artist.id, name: album_name)
-            if artist_album
-                # byebug
-                # if album exists, use it for song creation
-                song = Song.find_or_create_by(name: song_name, artist_id: artist.id, spotify_id: song_spotify_id, album_id: artist_album.id)
-                # genre 
-
-                if params[:genre_id]
-                    @post = Post.create(user_id: user_id, artist_id: artist.id, song_id: song.id, genre_id: genre_id)
-                    if features.length > 0
-                        features.each do |id|
-                            Postfeature.create(user_id: id, post_id: @post.id)
-                        end
-                    end
-                    if params[:instruments].kind_of?(Array)
-                        selected_instruments = JSON.parse params[:instruments]
-                        selected_instruments.each do |id|
-                            Postinstrument.create(instrument_id: id, post_id: @post.id)
-                        end
-                    else
-                        instrument = Instrument.find_or_create_by(name: params[:instruments])
-                        Postinstrument.create(instrument_id: instrument.id, post_id: @post.id)
-                    end
-                   
-                    # create the post first
-                    # then create postfeature and postinstruments instances
-                    @post.clip.attach(params[:clip])
-                    @post.thumbnail.attach(params[:thumbnail])
-                    render json: @post, serializer: PostSerializer
-                else 
-                    new_genre = Genre.find_or_create_by(name: params[:new_genre])
-                    @post = Post.create(user_id: user_id, artist_id: artist.id, song_id: song.id, genre_id: new_genre.id)
-                    if features.length > 0
-                        features.each do |id|
-                            Postfeature.create(user_id: id, post_id: @post.id)
-                        end
-                    end
-                    if params[:instruments].kind_of?(Array)
-                        selected_instruments = JSON.parse params[:instruments]
-                        selected_instruments.each do |id|
-                            Postinstrument.create(instrument_id: id, post_id: @post.id)
-                        end
-                    else
-                        instrument = Instrument.find_or_create_by(name: params[:instruments])
-                        Postinstrument.create(instrument_id: instrument.id, post_id: @post.id)
-                    end
-                   
-                    # create the post first
-                    # then create postfeature and postinstruments instances
-                    @post.clip.attach(params[:clip])
-                    @post.thumbnail.attach(params[:thumbnail])
-                    render json: @post, serializer: PostSerializer
-                end   
-            else
-                new_artist_album = Album.create(name: album_name, spotify_id: album_spotify_id, artist_id: artist.id)
-                new_song = Song.create(name: song_name, artist_id: artist.id, album_id: new_artist_album.id, spotify_id: song_spotify_id)
-                if params[:genre_id]
-                    @post = Post.create(user_id: user_id, artist_id: artist.id, song_id: new_song.id, genre_id: genre_id)
-                    if features.length > 0
-                        features.each do |id|
-                            Postfeature.create(user_id: id, post_id: @post.id)
-                        end
-
-                    end
-                    if params[:instruments].kind_of?(Array)
-                        selected_instruments = JSON.parse params[:instruments]
-                        selected_instruments.each do |id|
-                            Postinstrument.create(instrument_id: id, post_id: @post.id)
-                        end
-                    else
-                        instrument = Instrument.find_or_create_by(name: params[:instruments])
-                        Postinstrument.create(instrument_id: instrument.id, post_id: @post.id)
-                    end
-                    @post.clip.attach(params[:clip])
-                    @post.thumbnail.attach(params[:thumbnail])
-                    render json: @post, serializer: PostSerializer
-                else 
-                    new_genre = Genre.find_or_create_by(name: params[:new_genre])
-                    @post = Post.create(user_id: user_id, artist_id: artist.id, song_id: song.id, genre_id: new_genre.id)
-
-                    if features.length > 0
-                        features.each do |id|
-                            Postfeature.create(user_id: id, post_id: @post.id)
-                        end
-                    end
-
-                    if params[:instruments].kind_of?(Array)
-                        selected_instruments = JSON.parse params[:instruments]
-                        selected_instruments.each do |id|
-                            Postinstrument.create(instrument_id: id, post_id: @post.id)
-                        end
-                    else
-                        instrument = Instrument.find_or_create_by(name: params[:instruments])
-                        Postinstrument.create(instrument_id: instrument.id, post_id: @post.id)
-                    end
-                   
-                    # create the post first
-                    # then create postfeature and postinstruments instances
-                    @post.clip.attach(params[:clip])
-                    @post.thumbnail.attach(params[:thumbnail])
-                    render json: @post, serializer: PostSerializer
-                end
+        artist = Artist.find_or_create_by(name: artist_name, spotify_id: artist_spotify_id)
+        album = Album.find_or_create_by(name: album_name, spotify_id: album_spotify_id, artist_id: artist.id)
+        song = Song.find_or_create_by(name: song_name, artist_id: artist.id, spotify_id: song_spotify_id, album_id: album.id)
+        genre = Genre.find_or_create_by(name: params[:genre])
+        @post = Post.create(user_id: user_id, artist_id: artist.id, song_id: song.id, genre_id: genre.id)
+        if features.length > 0
+            features.each do |id|
+                Postfeature.create(user_id: id, post_id: @post.id)
             end
-            # if artist exists, find album by artist_id, album_spotify_id, and album_name
-            # if not create album then find song
-        else
-        # if not create an artist instance
-        # then create post instance
-            new_artist = Artist.create(name: artist_name, spotify_id: artist_spotify_id)
-            new_album = Album.create(name: album_name, spotify_id: album_spotify_id, artist_id: new_artist.id)
-            song = Song.find_or_create_by(name: song_name, artist_id: new_artist.id, spotify_id: song_spotify_id, album_id: new_album.id)
-
-            if params[:genre_id]
-                @post = Post.create(user_id: user_id, artist_id: new_artist.id, song_id: song.id, genre_id: genre_id)
-
-                if features.length > 0
-                    features.each do |id|
-                        Postfeature.create(user_id: id, post_id: @post.id)
-                    end
-                end
-
-                if params[:instruments].kind_of?(Array)
-                    selected_instruments = JSON.parse params[:instruments]
-                    selected_instruments.each do |id|
-                        Postinstrument.create(instrument_id: id, post_id: @post.id)
-                    end
-                else
-                    instrument = Instrument.find_or_create_by(name: params[:instruments])
-                    Postinstrument.create(instrument_id: instrument.id, post_id: @post.id)
-                end
-
-                @post.clip.attach(params[:clip])
-                @post.thumbnail.attach(params[:thumbnail])
-                render json: @post, serializer: PostSerializer
-
-            else
-                new_genre = Genre.find_or_create_by(name: params[:new_genre])
-                @post = Post.create(user_id: user_id, artist_id: artist.id, song_id: song.id, genre_id: new_genre.id)
-
-                    if features.length > 0
-                        features.each do |id|
-                            Postfeature.create(user_id: id, post_id: @post.id)
-                        end
-                    end
-
-
-                    if params[:instruments].kind_of?(Array)
-                        selected_instruments = JSON.parse params[:instruments]
-                        selected_instruments.each do |id|
-                            Postinstrument.create(instrument_id: id, post_id: @post.id)
-                        end
-                    else
-                        instrument = Instrument.find_or_create_by(name: params[:instruments])
-                        Postinstrument.create(instrument_id: instrument.id, post_id: @post.id)
-                    end
-                   
-                    # create the post first
-                    # then create postfeature and postinstruments instances
-                    @post.clip.attach(params[:clip])
-                    @post.thumbnail.attach(params[:thumbnail])
-                    render json: @post, serializer: PostSerializer
-                end
-
-
         end
-
+        if params[:instruments].kind_of?(Array)
+            selected_instruments = JSON.parse params[:instruments]
+            selected_instruments.each do |id|
+                Postinstrument.create(instrument_id: id, post_id: @post.id)
+            end
+        else
+            instrument = Instrument.find_or_create_by(name: params[:instruments])
+            Postinstrument.create(instrument_id: instrument.id, post_id: @post.id)
+        end
+        @post.clip.attach(params[:clip])
+        @post.thumbnail.attach(params[:thumbnail])
+        render json: @post, serializer: PostSerializer
     end 
-
-
-
-
-
-
 
     def filter
         instruments = params[:selected_instruments]
