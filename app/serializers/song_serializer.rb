ae63859917bcb3719d812c3ae5d1aecafdc2bdd7
mertoz41 <mertozka41@gmail.com>
2021-07-30 01:01:57 -0400

    song serializer updated

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,32 +1,36 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :name, :artist_name, :post_count, :user_count, :bandpost_count, :spotify_id, :album_name, :favoriteusers_count, :artist_id, :artistSpotifyId, :followingusers_count, :view_count
+  attributes :id, :name, :artist_name, :post_count, :user_count, :spotify_id, :album_name, :favoriteusers_count, :artist_id, :artistSpotifyId, :followingusers_count, :view_count
   has_many :posts
   has_many :bandposts
   
   def artist_name
     return object.artist.name
   end
+
   def artistSpotifyId
     return object.artist.spotify_id
   end
+
   def post_count
-    return object.posts.size
+    return object.posts.size + object.bandposts.size
   end
+  
   def user_count
     return object.users.size
   end
-  def bandpost_count
-    return object.bandposts.size
-  end
+
   def album_name
     return object.album.name
   end
+
   def favoriteusers_count
     return object.favoriteusers.size
   end
+
   def followingusers_count
     return object.followingusers.size
   end
+
   def view_count
     view = 0
     object.posts.each do |post|
@@ -37,5 +41,6 @@ class SongSerializer < ActiveModel::Serializer
     end
     return view
   end
+
 end
 
