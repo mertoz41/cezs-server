dc792448f599e5753450d7223d5e2d7eddc2d13f
mertoz41 <mertozka41@gmail.com>
2021-08-13 23:02:23 -0400

    login and check updated

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,14 +2,11 @@ class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-        # byebug
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
             payload = {user_id: @user.id}
             token = encode(payload)
             @timeline = []
-            # @posts = []
-            # @shares = @user.shares
             chatrooms = []
             @user.chatrooms.each do |room|
                 obj = {}
@@ -20,35 +17,35 @@ class AuthController < ApplicationController
             end 
 
             @user.posts.each do |post|
-                # @posts.push(post)
                 @timeline.push(post)
             end 
-            # @user.bands.each do |band|
-            #     band.bandposts.each do |bandpost|
-            #         @timeline.push(bandpost)
-            #     end
-            # end
+            @user.bands.each do |band|
+                band.posts.each do |bandpost|
+                    @timeline.push(bandpost)
+                end
+            end
             @user.followeds.each do |user|
                 user.posts.each do |post|
                 @timeline.push(post)
                 end 
             end
-            # @user.followedbands.each do |band|
-            #     band.bandposts.each do |bandpost|
-            #         @timeline.push(bandpost)
-            #     end
-            # end 
+            @user.followedbands.each do |band|
+                band.posts.each do |bandpost|
+                    @timeline.push(bandpost)
+                end
+            end 
             @user.followedartists.each do |artist|
                 artist.posts.each do |post|
                     @timeline.push(post)
                 end
-                # artist.bandposts.each do |post|
-                #     @timeline.push(post)
-                # end
+
             end 
-            # @user.userdescposts.each do |post|
-            #     @timeline.push(post)
-            # end
+            @user.followedsongs.each do |song|
+                song.posts.each do |post|
+                    @timeline.push(post)
+                end
+            end
+
 
             render json: {user: UserSerializer.new(@user), token: token, timeline: ActiveModel::Serializer::CollectionSerializer.new(@timeline, each_serializer: PostSerializer), chatrooms: chatrooms}
         else 
@@ -57,11 +54,9 @@ class AuthController < ApplicationController
     end
 
     def check
-        # byebug
         token = request.headers["Authorization"].split(' ')[1]
         @user = User.find(decode(token)["user_id"])
         @timeline = []
-        # @posts = []
             
         chatrooms = []
         @user.chatrooms.each do |room|
@@ -71,43 +66,30 @@ class AuthController < ApplicationController
             obj['users'] = ActiveModel::Serializer::CollectionSerializer.new(@room_users, each_serializer: UserSerializer)
             chatrooms.push(obj)
         end 
-        # @user.followedbands.each do |band|
-        #     band.bandposts.each do |bandpost|
-        #         @timeline.push(bandpost)
-        #     end
-        # end 
-        # @user.bands.each do |band|
-        #     band.bandposts.each do |bandpost|
-        #         @timeline.push(bandpost)
-        #     end
-        #     band.banddescposts.each do |banddescpost|
-        #         @timeline.push(banddescpost)
-        #     end
-        # end
+        @user.followedbands.each do |band|
+            band.posts.each do |bandpost|
+                @timeline.push(bandpost)
+            end
+        end 
+        @user.bands.each do |band|
+            band.posts.each do |bandpost|
+                @timeline.push(bandpost)
+            end
+        end
         
         @user.followedartists.each do |artist|
             artist.posts.each do |post|
                 @timeline.push(post)
             end
-            # artist.bandposts.each do |post|
-            #     @timeline.push(post)
-            # end
         end 
         
-        # @shares = @user.shares
             @user.posts.each do |post|
-                # @posts.push(post)
                 @timeline.push(post)
             end
-            @user.userdescposts.each do |post|
-                @timeline.push(post)
-            end
+           
             @user.followeds.each do |user|
                 user.posts.each do |post|
                 @timeline.push(post)
-                end
-                user.userdescposts.each do |post|
-                    @timeline.push(post)
                 end
             end 
         
