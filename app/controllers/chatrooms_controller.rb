179527a46d18b303694d630e3e1422563fdc3237
mertoz41 <mertozka41@gmail.com>
2021-09-20 00:09:03 -0400

    few tweaks on chatrooms controller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -12,8 +12,8 @@ class ChatroomsController < ApplicationController
       user_chatroom = Userchatroom.find_or_create_by(chatroom_id: chatroom.id, user_id: user.id)
       receiving_chatroom = Userchatroom.find_or_create_by(chatroom_id: chatroom.id, user_id: receiving_user.id)
       message = Message.create(content: params[:message], user_id: user.id, chatroom_id: chatroom.id)
-      serialized_data = ActiveModelSerializers::Adapter::Json.new(MessageSerializer.new(message)).serializable_hash
-      render json: {chatroom: chatroom}
+      # serialized_data = ActiveModelSerializers::Adapter::Json.new(MessageSerializer.new(message)).serializable_hash
+      render json: {chatroom: ChatroomSerializer.new(chatroom)}
       # ActionCable.server.broadcast "chatrooms_channel_#{chatroom.id}", serialized_data
       # head :ok
     # chatroom = Chatroom.create(title: params[:title])
