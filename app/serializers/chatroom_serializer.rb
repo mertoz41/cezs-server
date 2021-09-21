021b913b7c4633b4a59f53f8bd4d208d2e6c9959
mertoz41 <mertozka41@gmail.com>
2021-09-21 10:55:12 -0400

    last message on chatroom updated

--- app/serializers/chatroom_serializer.rb
+++ app/serializers/chatroom_serializer.rb
@@ -1,5 +1,5 @@
 class ChatroomSerializer < ActiveModel::Serializer
-  attributes :id, :last_message, :users, :last_message_time, :last_message_seen
+  attributes :id, :last_message, :users
   include Rails.application.routes.url_helpers
 
   def users
@@ -13,18 +13,18 @@ class ChatroomSerializer < ActiveModel::Serializer
 
   def last_message
     if object.messages.length > 0
-      return object.messages.last.content
+      return object.messages.last
     end
   end
   
-  def last_message_time
-    if object.messages.length > 0
-      return object.messages.last.created_at
-    end
-  end
-  def last_message_seen
-    if object.messages.length > 0
-      return object.messages.last.seen
-    end
-  end
+  # def last_message_time
+  #   if object.messages.length > 0
+  #     return object.messages.last.created_at
+  #   end
+  # end
+  # def last_message_seen
+  #   if object.messages.length > 0
+  #     return object.messages.last.seen
+  #   end
+  # end
 end
