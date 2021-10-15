9c118599f1d517d84b72a2b71f20e58a48fa7716
mertoz41 <mertozka41@gmail.com>
2021-10-15 14:56:59 -0400

    gig number included in locations index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,8 @@
 class LocationsController < ApplicationController
     def index
         @locations = Location.all
-        render json: {locations: ActiveModel::Serializer::CollectionSerializer.new(@locations, each_serializer: LocationSerializer), users_number: User.all.size}
+        events_number = Event.where('event_date >= ?', Date.today).size
+        render json: {locations: ActiveModel::Serializer::CollectionSerializer.new(@locations, each_serializer: LocationSerializer), users_number: User.all.size, events_number: events_number}
         # how can all locations except current users be shown so that loggedin users location isnt filtered out post process.
     end
     def show
