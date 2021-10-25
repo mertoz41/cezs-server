a209f0160f93c7fe053f001e69067fb497b75cfa
mertoz41 <mertozka41@gmail.com>
2021-10-24 21:21:45 -0400

    artist favoriting fixed

--- app/controllers/userartists_controller.rb
+++ app/controllers/userartists_controller.rb
@@ -1,44 +1,23 @@
 class UserartistsController < ApplicationController
     def create
         user = User.find(params[:user_id])
-        @artist = Artist.find_by(spotify_id: params[:artistSpotifyId])
-        
-        if @artist
-            user_artist = Userartist.create(user_id: user.id, artist_id: @artist.id)
-            render json: {artist: ArtistSerializer.new(@artist)}
-            # create userartist instance
-        else
-            @new_artist = Artist.create(name: params[:name], spotify_id: params[:artistSpotifyId])
-            user_artist = Userartist.create(user_id: user.id, artist_id: @new_artist.id)
-            render json: {artist: ArtistSerializer.new(@new_artist)}
-
-            # create artist first
-            # create user artist instance
-        end
+        artist = Artist.find_by(spotify_id: params[:spotify_id])
+        user_artist = Userartist.create(user_id: user.id, artist_id: artist.id)
+        render json: {artist_id: artist.id}
     end
+
     def delete
-        user = User.find(params[:user_id])
-        artist = Artist.find(params[:artist_id])
-        userartist = Userartist.find_by(user_id: user.id, artist_id: artist.id)
+        userartist = Userartist.find_by(user_id: params[:user_id], artist_id: params[:artist_id])
         userartist.destroy
-        if artist.posts.size == 0 && artist.bandposts.size == 0 && artist.userinfluences.size == 0 && artist.artistfollows.size == 0 && artist.userartists.size == 0
-            artist.destroy
-        end
         render json: {message: 'users favorite artist deleted.'}
     end
+
     def update
         user = User.find(params[:user_id])
         old_favorite = Userartist.find_by(artist_id: params[:oldArtistId], user_id: user.id)
         old_favorite.destroy
-        @artist = Artist.find_by(spotify_id: params[:artistSpotifyId])
-        if @artist
-            new_favorite = Userartist.create(artist_id: @artist.id, user_id: user.id)
-            render json: {artist: ArtistSerializer.new(@artist)}
-        else
-            @new_artist = Artist.create(name: params[:name], spotify_id: params[:artistSpotifyId])
-            new_favorite = Userartist.create(artist_id: @new_artist.id, user_id: user.id)
-            render json: {artist: ArtistSerializer.new(@new_artist)}
-        end
-        
+        artist = Artist.find_or_create_by(name: params[:name], spotify_id: params[:spotify_id])
+        new_favorite = Userartist.create(artist_id: artist.id, user_id: user.id)
+        render json: {artist_id: artist.id}
     end
 end
