6e60198421a70244576ed109d364f9cccfc347f7
bob <mertozka41@gmail.com>
2022-01-12 23:44:36 -0500

    commented code erased

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-  require "base64"
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     user = User.find(params[:user_id])
@@ -15,26 +14,6 @@ class MessagesController < ApplicationController
         ChatroomSerializer.new(@chatroom).as_json
       )
     end
-
-    # find other user that is outside this user,
-    # check if it has token if so send notification
-
-    # find chatrooms other users
-    # check if they have token
-    # if so send notifications
-    # channels = Redis.new.pubsub('channels', 'action_cable/*')
-    # tokens = channels.map do |channel|
-    #   channel.delete('action_cable/')
-    # end
-    # plains = []
-    # tokens.each do |token|
-    #   plains.push(Base64.decode64(token))
-    # end/
-    # byebug
-    # can i know if user is connected to the channel
-    # serialized_data = ActiveModelSerializers::Adapter::Json.new(MessageSerializer.new(message)).serializable_hash
-    # MessagesChannel.broadcast_to chatroom, message
-    # MessagesChannel.broadcast_to(chatroom, serialized_data)
     ActionCable.server.broadcast "chatrooms_channel_#{params[:chatroom_id]}", message
 
     head :ok
