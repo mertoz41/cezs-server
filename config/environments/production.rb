ecda450c637cccb51536771f3a5b00a2cdc490d3
mert <mertozka41@gmail.com>
2023-05-22 15:35:39 -0400

    actionmailer added to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_options = {from: 'support@cezsmusic.com'}
+  config.action_mailer.default_url_options = { :host => "cezsmusic.com" }
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
