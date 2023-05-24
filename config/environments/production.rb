31131c316d0962133249f214486247a0df9d008d
mert <mertozka41@gmail.com>
2023-05-23 23:40:21 -0400

    couple tricks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  config.require_master_key = true 
+  # config.require_master_key = true 
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -83,7 +83,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_options = {from: 'support@cezsmusic.com'}
   config.action_mailer.default_url_options = { :host => "cezsmusic.com" }
-  Rails.application.routes.default_url_options[:host] = "agile-hollows-87633.herokuapp.com"
+  # Rails.application.routes.default_url_options[:host] = "agile-hollows-87633.herokuapp.com"
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
