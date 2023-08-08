82f6cf05154a6d0560ca43263c3d594932cb72f8
mert <mertozka41@gmail.com>
2023-08-07 21:41:26 -0400

    last one

--- vendor/bundle/ruby/2.6.0/gems/activestorage-6.1.4.1/lib/active_storage/attached.rb
+++ vendor/bundle/ruby/2.6.0/gems/activestorage-6.1.4.1/lib/active_storage/attached.rb
@@ -11,11 +11,11 @@ module ActiveStorage
     def initialize(name, record)
       @name, @record = name, record
     end
-    def self.ransackable_attributes(auth_object = nil)
+    def ransackable_attributes(auth_object = nil)
       column_names + _ransackers.keys
     end
   
-    def self.ransackable_associations(auth_object = nil)
+    def ransackable_associations(auth_object = nil)
       reflect_on_all_associations.map { |a| a.name.to_s } + _ransackers.keys
     end
 
