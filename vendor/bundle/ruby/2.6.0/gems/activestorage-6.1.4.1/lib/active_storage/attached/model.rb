b77ed22d6f9deff8a6a2433a801cdd2b3868e812
mert <mertozka41@gmail.com>
2023-08-07 21:30:15 -0400

    again

--- vendor/bundle/ruby/2.6.0/gems/activestorage-6.1.4.1/lib/active_storage/attached/model.rb
+++ vendor/bundle/ruby/2.6.0/gems/activestorage-6.1.4.1/lib/active_storage/attached/model.rb
@@ -8,8 +8,12 @@ module ActiveStorage
     extend ActiveSupport::Concern
 
     def self.ransackable_attributes(auth_object = nil)
-      ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
-  end
+      column_names + _ransackers.keys
+    end
+  
+    def self.ransackable_associations(auth_object = nil)
+      reflect_on_all_associations.map { |a| a.name.to_s } + _ransackers.keys
+    end
     class_methods do
       # Specifies the relation between a single attachment and the model.
       #
